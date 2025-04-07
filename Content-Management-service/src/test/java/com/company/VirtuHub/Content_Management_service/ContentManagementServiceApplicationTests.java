f66a9061822fd08ac2db06d37dde788b00a4ea87
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-07 15:02:40 +0530

    added the content-Management-service

---
+++ Content-Management-service/src/test/java/com/company/VirtuHub/Content_Management_service/ContentManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.VirtuHub.Content_Management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
