46006c74b84aa3ed1fa1c5c4efd2220865b2480f
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-09 08:11:36 +0530

    added the entity class

---
+++ Payment-service/src/main/java/com/company/VirtuHub/Payment_service/entities/PaymentTransaction.java
@@ -0,0 +1,36 @@
+package com.company.VirtuHub.Payment_service.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payment_transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long eventId;
+
+    private Long userId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private String PaymentMethod;
+    private String transactionReference;
+
+    private LocalDateTime paymentTime;
+    private Boolean isReceiptGenerated;
+}
